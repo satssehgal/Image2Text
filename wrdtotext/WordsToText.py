5233060f3475d82d8005366dd9e1b2c14b48cbeb
satssehgal <satssehgal@gmail.com>
2019-06-02 08:45:38 -0400

    Update WordsToText.py

--- wrdtotext/WordsToText.py
+++ wrdtotext/WordsToText.py
@@ -3,6 +3,8 @@ import pytesseract
 import re
 import urllib.request
 import numpy as np
+import os
+import pandas as pd
 
 def url_to_image(url):
     resp = urllib.request.Request(url, headers={'User-Agent': "Magic Browser"})
@@ -21,4 +23,19 @@ def img2text(URL):
     text = pytesseract.image_to_string(im, config=config)
     text2 = text.rstrip('\n')
     return (text2)
+
+def img2textdir(dirpath):
+    files = []
+    for file in os.listdir(dirpath):
+        if file.endswith('.jpg') or file.endswith('.png'):
+            files.append(os.path.join(dirpath, file))
+    texts=[]
+    names=[]
+    for i in files:
+        text=img2text(i)
+        texts.append(text)
+        names.append(i)
+    translation={'Image':names, 'Text':texts}
+    df=pd.DataFrame(data=translation)
+    return df
 
