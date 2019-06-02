af01b36e4fb9139689cdbfff3acaa541b5cc2015
satssehgal <satssehgal@gmail.com>
2019-06-02 08:08:05 -0400

    Update WordsToText.py

--- wrdtotext/WordsToText.py
+++ wrdtotext/WordsToText.py
@@ -8,18 +8,17 @@ def url_to_image(url):
     resp = urllib.request.Request(url, headers={'User-Agent': "Magic Browser"})
     con = urllib.request.urlopen(resp)
     image = np.asarray(bytearray(con.read()), dtype="uint8")
-    image2 = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image2 = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
     return image2
 
 def img2text(URL):
     if re.findall('^http', URL):
         im = url_to_image(URL)
     else:
-        im = cv2.imread(URL, cv2.IMREAD_COLOR)
+        img = cv2.imread(URL)
+        im = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     config = ('-l eng --oem 1 --psm 6')
     text = pytesseract.image_to_string(im, config=config)
     text2 = text.rstrip('\n')
     return (text2)
-
-
 
